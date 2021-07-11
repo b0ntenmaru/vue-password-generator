b3ad26700e5f235d8ae1dec1ae2e2faea7cf3419
Hiroaki Date <mmst3l4@gmail.com>
2021-07-11 18:23:43 +0900

    vuetifyをinstall

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,16 @@
 <template>
-  <div class="home">hogehoge</div>
+  <hello-world />
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import HelloWorld from "../components/HelloWorld.vue";
 
 export default Vue.extend({
   name: "Home",
+
+  components: {
+    HelloWorld,
+  },
 });
 </script>
