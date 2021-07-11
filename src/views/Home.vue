0c652ab0f526ff0a130e4b103170e1ed5408dea7
Hiroaki Date <mmst3l4@gmail.com>
2021-07-11 18:27:53 +0900

    不要なファイルを削除

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,11 @@
 <template>
-  <hello-world />
+  <div class="home">hogehoge</div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import HelloWorld from "../components/HelloWorld.vue";
 
 export default Vue.extend({
   name: "Home",
-
-  components: {
-    HelloWorld,
-  },
 });
 </script>
